a1d06f2110fbf3604de7b5d4c03b9329656dd582
hermes3306 <bstyfs23@hotmail.com>
2025-09-30 20:53:35 +0900

    unlimited downlioad

--- faces/face_collector.py
+++ faces/face_collector.py
@@ -556,7 +556,7 @@ def main():
     import argparse
 
     parser = argparse.ArgumentParser(description='Face Collection and Embedding System')
-    parser.add_argument('--count', type=int, default=10,
+    parser.add_argument('--count', type=int, default=1000000,
                        help='Number of faces to download (default: 10)')
     args = parser.parse_args()
 
